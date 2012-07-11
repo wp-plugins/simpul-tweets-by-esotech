efbbbb7628577371eb935d49067e362df3ae368b
geilt <geilt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-11 22:50:33 +0000

    Added caching via SimpulFacebook implementation.
    git-svn-id: https://plugins.svn.wordpress.org/simpul-tweets-by-esotech/trunk@570954 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,16 +1,18 @@
 === Plugin Name ===
 Contributors: geilt
 Donate link: http://www.esotech.org/
-Tags: twitter, feed, widget
+Tags: twitter, feed, widget, tweets, simpul, esotech, json
 Requires at least: 3.4
 Tested up to: 3.4
 Stable tag: trunk
 
 == Description ==
 
-Enables a widget that will pull a twitter feed via JSON and display text and/or images in posts.
+Enables a widget that will pull a twitter feed feed via JSON by Twitter Name and display them. 
 
 == Changelog ==
 
+= 1.5 =
+* Added caching.
 = 1.0 =
 * First Upload
--- simpultweets.php
+++ simpultweets.php
@@ -6,7 +6,7 @@
 Plugin Name: Simpul Tweets by Esotech
 Plugin URI: http://www.esotech.org
 Description: This plugin is designed to access a twitter feed and display it in a Wordpress Widget.
-Version: 1.0
+Version: 1.5
 Author: Alexander Conroy
 Author URI: http://www.esotech.org/people/alexander-conroy/
 License: Commercial
@@ -34,6 +34,24 @@ class SimpulTweets extends WP_Widget
 		if( $instance['title'] ):
 			$widget .= '<h2 class="widgettitle">' . $instance['title'] . '</h2>';
 		endif;
+		
+		// Solution for caching.
+		if($instance['cache_enabled']):
+		
+			$widget_class = explode('-', $args['widget_id']);
+			$widget_id = $widget_class[count($widget_class) - 1];
+			$widget_array = get_option('widget_simpul_tweets');
+			
+			if(!$instance['cache'] || current_time('timestamp') > strtotime($instance['cache_interval'] . ' hours', $instance['last_cache_time'])):
+				$instance['cache'] = self::getTweets( $instance['account'], $instance['number'] );;
+				$instance['last_cache_time'] = current_time('timestamp');
+			endif;
+			
+			$widget_array[$widget_id] = $instance;
+			update_option('widget_simpul_facebook', $widget_array);
+			
+		endif;
+		
 		$widget .= self::getTweets( $instance['account'], $instance['number'] );
 		$widget .= "</li>";
 		
@@ -54,6 +72,8 @@ class SimpulTweets extends WP_Widget
 		$instance['title'] 		= strip_tags($new_instance['title']);
 		$instance['account'] 	= strip_tags($new_instance['account']);
 		$instance['number'] 	= strip_tags($new_instance['number']);
+		$instance['cache_enabled']			= strip_tags($new_instance['cache_enabled']);
+		$instance['cache_interval']			= strip_tags($new_instance['cache_interval']);
 		
 		return $instance;
 	}
@@ -70,6 +90,8 @@ class SimpulTweets extends WP_Widget
 		$title 		= strip_tags($instance['title']);
 		$account 	= strip_tags($instance['account']);
 		$number 	= strip_tags($instance['number']);
+		$cache_enabled 				= strip_tags($instance['cache_enabled']);
+		$cache_interval 			= strip_tags($instance['cache_interval']);
 		?>
 			<p>
 				<label for="<?php echo $this->get_field_id('title'); ?>">
@@ -89,8 +111,10 @@ class SimpulTweets extends WP_Widget
 					<input class="widefat" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo attribute_escape($number); ?>" />
 				</label>
 			</p>
-					
 		<?php
+		echo "<h3>Cache Options</h3>";
+		echo self::formatField($this->get_field_name('cache_enabled'), $this->get_field_id('cache_enabled'), $cache_enabled, "Use Cache?", 'checkbox' );
+		echo self::formatField($this->get_field_name('cache_interval'), $this->get_field_id('cache_interval'),  $cache_interval, "Cache Interval (hours)" );
 	}
 
 	# -----------------------------------------------------------------------------#
@@ -123,7 +147,11 @@ class SimpulTweets extends WP_Widget
 		$i = null;
 
 		# get the actual feed
-		$tweep = self::twitterStatus($twitter_id);
+		if($instance['cache_enabled']):
+			$tweep = $instance['cache'];
+		else:
+			$tweep = self::twitterStatus($twitter_id);
+		endif;
 		//print_r($tweep);
 	
 		# Make sure we have something to work with
@@ -162,6 +190,34 @@ class SimpulTweets extends WP_Widget
 		$text  = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})', '<a href="mailto:\\1">\\1</a>', $text );
 		
 		return $text;
+	}
+	public function formatField($field, $id, $value, $description, $type = "text", $args = array(), $options = array() )	{
+		if($type == "text"):
+			return '<p>
+					<label for="' . $id . '">
+						' . $description . ': 
+						<input class="widefat" id="' . $id . '" name="' . $field. '" type="text" value="' . attribute_escape($value) . '" />
+					</label>
+					</p>';
+		elseif($type == "checkbox"):
+			if( $value ) $checked = "checked";
+			return '<p>
+					<label for="' . $field . '">
+						
+						<input id="' . $field. '" name="' . $field . '" type="checkbox" value="1" ' . $checked . ' /> ' . $description . ' 
+					</label>
+					</p>';
+		elseif($type == "radio"):
+			$radio = '<p>
+					<label for="' . $field . '">' . $description . '<br />';
+					foreach($options as $option):
+						if( $value == $option ): $checked = "checked"; else: $checked = ""; endif;						
+						$radio .= '<input id="' . $field. '" name="' . $field . '" type="radio" value="' . $option . '" ' . $checked . ' /> ' . SimpulEvents::getLabel($option) . '<br />';
+					endforeach; 
+			$radio .= '</label>
+					</p>';
+			return $radio;
+		endif;
 	}
 }
 //Register the Widget
