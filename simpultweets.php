5a8c6ca2bedb67462da314e2896aa08974017999
geilt <geilt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-11 07:42:09 +0000

    Updated due to twitter feed format change.
    git-svn-id: https://plugins.svn.wordpress.org/simpul-tweets-by-esotech/trunk@610647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simpultweets.php
+++ simpultweets.php
@@ -6,10 +6,10 @@
 Plugin Name: Simpul Tweets by Esotech
 Plugin URI: http://www.esotech.org
 Description: This plugin is designed to access a twitter feed and display it in a Wordpress Widget.
-Version: 1.6
+Version: 1.8
 Author: Alexander Conroy
 Author URI: http://www.esotech.org/people/alexander-conroy/
-License: Commercial
+License: This code is released under the GPL licence version 3 or later, available here http://www.gnu.org/licenses/gpl.txt
 */
 
 
@@ -94,6 +94,11 @@ class SimpulTweets extends WP_Widget
 		$instance['number'] 				= strip_tags($new_instance['number']);
 		$instance['tweet_class'] 			= strip_tags($new_instance['tweet_class']);
 		$instance['tweet_element'] 			= strip_tags($new_instance['tweet_element']);
+		$instance['tweet_element_link'] 	= strip_tags($new_instance['tweet_element_link']);
+		$instance['tweet_date'] 			= strip_tags($new_instance['tweet_date']);
+		$instance['tweet_date_format']		= strip_tags($new_instance['tweet_date_format']);
+		$instance['tweet_date_link'] 		= strip_tags($new_instance['tweet_date_link']);
+		
 		$instance['cache_enabled']			= strip_tags($new_instance['cache_enabled']);
 		$instance['cache_interval']			= strip_tags($new_instance['cache_interval']);
 		
@@ -115,6 +120,10 @@ class SimpulTweets extends WP_Widget
 		$number 					= strip_tags($instance['number']);
 		$tweet_class				= strip_tags($instance['tweet_class']);
 		$tweet_element				= strip_tags($instance['tweet_element']);
+		$tweet_element_link			= strip_tags($instance['tweet_element_link']);
+		$tweet_date					= strip_tags($instance['tweet_date']);
+		$tweet_date_format			= strip_tags($instance['tweet_date_format']);
+		$tweet_date_link			= strip_tags($instance['tweet_date_link']);
 		$cache_enabled 				= strip_tags($instance['cache_enabled']);
 		$cache_interval 			= strip_tags($instance['cache_interval']);
 		?>
@@ -126,6 +135,10 @@ class SimpulTweets extends WP_Widget
 		echo self::formatField($this->get_field_name('number'), $this->get_field_id('number'), $number, "Amount of tweets to be displayed: " );
 		echo self::formatField($this->get_field_name('tweet_class'), $this->get_field_id('tweet_class'), $tweet_class, "Tweet Container Class" );
 		echo self::formatField($this->get_field_name('tweet_element'), $this->get_field_id('tweet_element'), $tweet_element, "Tweet Element(default p)" );
+		echo self::formatField($this->get_field_name('tweet_element_link'), $this->get_field_id('tweet_element_link'), $tweet_element_link, "Link Element to Tweet", 'checkbox' );
+		echo self::formatField($this->get_field_name('tweet_date'), $this->get_field_id('tweet_date'), $tweet_date, "Show Tweet Date", 'checkbox' );
+		echo self::formatField($this->get_field_name('tweet_date_format'), $this->get_field_id('tweet_date_format'), $tweet_date_format, "Date Format" );
+		echo self::formatField($this->get_field_name('tweet_date_link'), $this->get_field_id('tweet_date_link'), $tweet_date_link, "Link Tweet Date", 'checkbox' );
 		echo "<h3>Cache Options</h3>";
 		echo self::formatField($this->get_field_name('cache_enabled'), $this->get_field_id('cache_enabled'), $cache_enabled, "Use Cache?", 'checkbox' );
 		echo self::formatField($this->get_field_name('cache_interval'), $this->get_field_id('cache_interval'),  $cache_interval, "Cache Interval (hours)" );
@@ -138,16 +151,26 @@ class SimpulTweets extends WP_Widget
 	# -----------------------------------------------------------------------------#
 	# Get the twitter feed via CURL according to the Twitter ID
 	# -----------------------------------------------------------------------------#
-	public function twitterStatus($twitter_id)
+	public function twitterStatus($twitter_id, $instance = null)
 	{
-		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, "https://twitter.com/statuses/user_timeline/".$twitter_id.".json");
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 3);
-		curl_setopt($c, CURLOPT_TIMEOUT, 5);
-		$response = curl_exec($c);
-		$responseInfo = curl_getinfo($c);
-		curl_close($c);
+		$ch = curl_init();
+		$header = array();
+		$header[] = 'Accept: text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5';
+		$header[] = 'Cache-Control: max-age=0';
+		$header[] = 'Connection: keep-alive';
+		$header[] = 'Keep-Alive: 300';
+		$header[] = 'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7';
+		$header[] = 'Accept-Language: en-us,en;q=0.5';
+		$header[] = 'Pragma: ';
+		//curl_setopt($c, CURLOPT_URL, "https://twitter.com/statuses/user_timeline/".$twitter_id.".json");
+		curl_setopt($ch, CURLOPT_URL, "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=" . $twitter_id . "&count=" . $instance['number'] );
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		$response = curl_exec($ch);
+		$responseInfo = curl_getinfo($ch);
+		curl_close($ch);
 
 		return json_decode($response);
 	}
@@ -161,29 +184,25 @@ class SimpulTweets extends WP_Widget
 		$i = null;
 
 		$twitter_id = $instance['account'];
-		$total_tweets = $instance['number'] ? $instance['number'] : 20;
+		$instance['number'] = $instance['number'] ? $instance['number'] : 20;
+		$instance['tweet_date_format'] = $instance['tweet_date_format'] ? $instance['tweet_date_format'] : "Y-m-d H:i:s";
+		$instance['tweet_element'] = $instance['tweet_element'] ? $instance['tweet_element'] : "p";
 		
 		# get the actual feed
 		if($instance['cache_enabled']):
 			$tweep = $instance['cache'];
 		else:
-			$tweep = self::twitterStatus($twitter_id);
+			$tweep = self::twitterStatus($twitter_id, $instance);
 		endif;
 		//print_r($tweep);
 	
 		# Make sure we have something to work with
-		if(!empty($tweep)):
-			
-			$tweet_count = 1;
+		if(!empty($tweep)):	
 			foreach($tweep as $tweet):
-				if($tweet_count > $total_tweets)
-					break;
-				$tweet_count++;
-
 				$tweet_link = "https://twitter.com/" . $twitter_id . "/status/" . $tweet->id;
 				
-				if( !empty( $instance['tweet_date'] ) ):
-					$tweet_date = date ("Y-m-d H:i:s", strtotime($tweet->created_at . " -5 Hours"));
+				if( !empty( $instance['tweet_date'] ) ):	
+					$tweet_date = date($instance['tweet_date_format'], strtotime($tweet->created_at . " -5 Hours"));
 					if( !empty($instance['tweet_date_link'] ) ):
 						$tweet_date ='<a href="' . $tweet_link . '" target="_blank">' . $tweet_date . '</a>';	
 					endif;
@@ -192,12 +211,15 @@ class SimpulTweets extends WP_Widget
 				$content = $tweet->text;
 				
 				$content = self::textToLink($content);
-					
-				if( !empty( $instance['tweet_element'] ) ):
-					$tweets .= '<' . $instance['tweet_element'] . '>'. $content." - " . $tweet_date  . '</' . $instance['tweet_element'] . '>'; 
+				if($instance['tweet_element_link']):
+					$link_start = '<a href="' . $tweet_link . '">';
+					$link_end = '</a>';
 				else:
-					$tweets .= "<p>".$content." - " . $tweet_date . "</p>";
+					$link_start = '';
+					$link_end = '';
 				endif;
+				$tweets .= '<' . $instance['tweet_element'] . '>' . $link_start . $content." - " . $tweet_date  . $link_end . '</' . $instance['tweet_element'] . '>';
+				
 			endforeach;
 			return $tweets;
 		endif;
